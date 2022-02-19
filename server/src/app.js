62a7c8cd5b2352e9b463b292c80fbb67db84918e
jam-madrigal <jam.madrigal@gmail.com>
2022-02-18 21:25:14 -0800

    Add cors middleware

--- server/src/app.js
+++ server/src/app.js
@@ -1,9 +1,15 @@
+// Dependencies
 const express = require('express');
-const app = express();
+const cors = require('cors');
+
 const planetsRouter = require('./routes/planets/planets.router');
 
+const app = express();
+// Middleware
+app.use(cors({
+    origin: 'http://localhost:3000',
+}));
 app.use(express.json());
-
 app.use(planetsRouter);
 
 module.exports = app;
