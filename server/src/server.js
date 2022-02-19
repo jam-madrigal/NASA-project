a6a037033bb88b5d50959eb2a65ab2586d68882a
jam-madrigal <jam.madrigal@gmail.com>
2022-02-18 20:30:22 -0800

    Setup server

---
+++ server/src/server.js
@@ -0,0 +1,16 @@
+const http = require('http');
+
+const app = require('./app')
+
+const PORT = process.env.PORT || 8000;
+
+const server = http.createServer(app);
+
+server.listen(PORT, () => {
+    console.log(`Server is listening on ${PORT}...`)
+})
+// 
+
+// app.listen();
+
+// Check if there is a port specified in the environment, otherwise default to port 8000, as to not conflict with 3000 with the react app
