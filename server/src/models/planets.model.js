83f0e53cba0d17d9dbb37020ecbbc99d66400370
jam-madrigal <jam.madrigal@gmail.com>
2022-02-19 18:28:33 -0800

    Fix kepler data path

--- server/src/models/planets.model.js
+++ server/src/models/planets.model.js
@@ -1,5 +1,6 @@
-const { parse } = require('csv-parse'); 
 const fs = require('fs');
+const path = require('path');
+const { parse } = require('csv-parse'); 
 
 // Array to store the results of our kepler data read with node's api
 const habitablePlanets = [];
@@ -17,7 +18,7 @@ function isHabitablePlanet(planet) {
 function loadPlanetsData() {
     // Using our parsing function in a promise so we can wait for it to finish before our server moves on
     return new Promise((resolve, reject) => {
-        fs.createReadStream('kepler_data.csv')
+        fs.createReadStream(path.join(__dirname, '..', '..', 'data', 'kepler_data.csv'))
         .pipe(parse({
             // Labeling that lines starting with # are comments, because different methods may handle parsing the data differently, and adding the columns key to return each row as a javascript object, this will make it return key value pairs instead of an array with the data values in each row
             comment: "#",
