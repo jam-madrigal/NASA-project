4457b66e571548245ada31c3a48775c2f5bf4761
jam-madrigal <jam.madrigal@gmail.com>
2022-02-19 20:06:05 -0800

    Add root path serving of index.html

--- server/src/app.js
+++ server/src/app.js
@@ -14,5 +14,8 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, '../public')));
 
 app.use(planetsRouter);
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
+});
 
 module.exports = app;
