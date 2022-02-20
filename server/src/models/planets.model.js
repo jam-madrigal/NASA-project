f041ddacd68e92b1038ad029aceca1c05359e5b6
jam-madrigal <jam.madrigal@gmail.com>
2022-02-19 18:18:42 -0800

    Update planets model functions to use promises, await them in server.js

--- server/src/models/planets.model.js
+++ server/src/models/planets.model.js
@@ -14,31 +14,34 @@ function isHabitablePlanet(planet) {
 
 // Using node to read our kepler data csv file, adding handlers to push the resulting habitable planets to an array and log them, as well as catch any errors
 // .pipe will send our kepler data to the parse function, piping similar to terminal commands like in linux, a readable stream (createReadStream()) providing the input for a writable stream (parse())
-fs.createReadStream('kepler_data.csv')
-    .pipe(parse({
-        // Labeling that lines starting with # are comments, because different methods may handle parsing the data differently, and adding the columns key to return each row as a javascript object, this will make it return key value pairs instead of an array with the data values in each row
-        comment: "#",
-        columns: true
-    }))
-    .on('data', (data) => {
-        if (isHabitablePlanet(data)) {
-            habitablePlanets.push(data);
-        }
-    })
-    .on('error', (err) => {
-        console.log(err);
-    })
-    .on('end', () => {
-        // Filter by names of planet to make it more readable, and return the number of matching results
-        console.log(habitablePlanets.map((planets) => {
-            return planets['kepler_name'];
-        }));
-        console.log(`Found ${habitablePlanets.length} habitable planets.`)
-        console.log("Done reading data stream")
-    })
+function loadPlanetsData() {
+    // Using our parsing function in a promise so we can wait for it to finish before our server moves on
+    return new Promise((resolve, reject) => {
+        fs.createReadStream('kepler_data.csv')
+        .pipe(parse({
+            // Labeling that lines starting with # are comments, because different methods may handle parsing the data differently, and adding the columns key to return each row as a javascript object, this will make it return key value pairs instead of an array with the data values in each row
+            comment: "#",
+            columns: true
+        }))
+        .on('data', (data) => {
+            if (isHabitablePlanet(data)) {
+                habitablePlanets.push(data);
+            }
+        })
+        .on('error', (err) => {
+            console.log(err);
+            reject(err);
+        })
+        .on('end', () => {
+            console.log(`Found ${habitablePlanets.length} habitable planets.`)
+            console.log("Done reading data stream")
+            resolve();
+        });
+    });
+}
     
-
 module.exports = {
-    habitablePlanets,
+    loadPlanetsData,
+    planets: habitablePlanets
 
 }
--- server/src/server.js
+++ server/src/server.js
@@ -1,11 +1,17 @@
 const http = require('http');
 
-const app = require('./app')
+const app = require('./app');
 
+const { loadPlanetsData } = require('./models/planets.model');
+
+const planetsModel = require('./models/planets.model');
 // Check if there is a port specified in the environment, otherwise default to port 8000, as to not conflict with 3000 with the react app
 const PORT = process.env.PORT || 8000;
 
 const server = http.createServer(app);
+
+// Using our kepler stream data we converted to a promise so the data is aways available when our server starts
+await loadPlanetsData;
 
 server.listen(PORT, () => {
     console.log(`Server is listening on ${PORT}...`)
