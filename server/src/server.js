77f84e4aa0fb6ec3e7f48461c699addec01d002a
jam-madrigal <jam.madrigal@gmail.com>
2022-02-19 18:41:22 -0800

    Fix typo in async server function

--- server/src/server.js
+++ server/src/server.js
@@ -4,7 +4,6 @@ const app = require('./app');
 
 const { loadPlanetsData } = require('./models/planets.model');
 
-const planetsModel = require('./models/planets.model');
 // Check if there is a port specified in the environment, otherwise default to port 8000, as to not conflict with 3000 with the react app
 const PORT = process.env.PORT || 8000;
 
@@ -12,7 +11,7 @@ const server = http.createServer(app);
 
 // Using our kepler stream data we converted to a promise so the data is aways available when our server starts, we have to wrap it in a new function or else using await will cause an error since it is being used in the top level of a module/file
 async function startServer() {
-    await loadPlanetsData;
+    await loadPlanetsData();
     
     server.listen(PORT, () => {
         console.log(`Server is listening on ${PORT}...`)
