10724ad73ec796de3091c5c43a773c83e025adfa
jam-madrigal <jam.madrigal@gmail.com>
2022-02-20 17:43:58 -0800

    Add getAllLaunches function

---
+++ server/src/routes/launches/launches.controller.js
@@ -0,0 +1,10 @@
+const { launches } = require('../../models/launches.model');
+
+// Getting our launches data, converting it into an iterable list of values in the Map, and creating an array from that list
+function getAllLaunches(req, res) {
+    return res.status(200).json(Array.from(launches.values()));
+}
+
+module.exports = {
+    getAllLaunches
+}
