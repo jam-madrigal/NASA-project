595e824d72756d7bf89c9c5bcf46fe49d13219b9
jam-madrigal <jam.madrigal@gmail.com>
2022-02-20 17:10:37 -0800

    Add morgan middleware

--- server/src/app.js
+++ server/src/app.js
@@ -11,9 +11,9 @@ const app = express();
 app.use(cors({
     origin: 'http://localhost:3000',
 }));
+app.use(morgan('combined'));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '../public')));
-app.use(morgan('combined'));
 
 app.use(planetsRouter);
 
