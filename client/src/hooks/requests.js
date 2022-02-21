aa3fa35de2fa5269853a22271443289465a560d0
jam-madrigal <jam.madrigal@gmail.com>
2022-02-20 18:14:41 -0800

    Update requests.js with get and sort launches function

--- client/src/hooks/requests.js
+++ client/src/hooks/requests.js
@@ -1,16 +1,20 @@
 // All of our API functions will be made against our single API, so let's set our address of the API into a constant
 const API_URL = 'http://localhost:8000';
 
+// Use the browsers built in function, and specify that we are using different ports for our front and backend
+// Load planets and return as JSON.
 async function httpGetPlanets() {
-  // Use the browsers built in function, and specify that we are using different ports for our front and backend
-  // Load planets and return as JSON.
   const response = await fetch(`${API_URL}/planets`);
   return await response.json();
 }
 
+// Load launches, sort by flight number, and return as JSON.
 async function httpGetLaunches() {
-  // TODO: Once API is ready.
-  // Load launches, sort by flight number, and return as JSON.
+  const response = await fetch(`${API_URL}/launches`);
+  const fetchedLaunches = await response.json();
+  return fetchedLaunches.sort((a, b) => {
+    return a.flightNumber - b.flightNumber;
+  });
 }
 
 async function httpSubmitLaunch(launch) {
