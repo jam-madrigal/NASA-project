371ab47570756edfb924b375020595de2051847d
jam-madrigal <jam.madrigal@gmail.com>
2022-02-20 22:56:27 -0800

    Update getAllLaunches function to abstract converting the data away from launches controller, update function names

--- server/src/routes/launches/launches.controller.js
+++ server/src/routes/launches/launches.controller.js
@@ -1,10 +1,10 @@
-const { launches } = require('../../models/launches.model');
+const { getAllLaunches } = require('../../models/launches.model');
 
 // Getting our launches data, converting it into an iterable list of values in the Map, and creating an array from that list
-function getAllLaunches(req, res) {
-    return res.status(200).json(Array.from(launches.values()));
+function httpGetAllLaunches(req, res) {
+    return res.status(200).json(getAllLaunches);
 }
 
 module.exports = {
-    getAllLaunches
+    httpGetAllLaunches
 }
