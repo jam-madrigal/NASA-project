db9ddad8099c2c6a38af0df07d408bd12a15d34c
jam-madrigal <jam.madrigal@gmail.com>
2022-02-20 23:05:31 -0800

    Fix calling of getAllLaunches function

--- server/src/routes/launches/launches.controller.js
+++ server/src/routes/launches/launches.controller.js
@@ -2,7 +2,7 @@ const { getAllLaunches } = require('../../models/launches.model');
 
 // Getting our launches data, converting it into an iterable list of values in the Map, and creating an array from that list
 function httpGetAllLaunches(req, res) {
-    return res.status(200).json(getAllLaunches);
+    return res.status(200).json(getAllLaunches());
 }
 
 module.exports = {
