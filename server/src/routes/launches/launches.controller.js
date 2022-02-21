09f52b18449dc859b3780242e47a9d7b03a4c27f
jam-madrigal <jam.madrigal@gmail.com>
2022-02-21 00:06:35 -0800

    Add validation for POST requests

--- server/src/routes/launches/launches.controller.js
+++ server/src/routes/launches/launches.controller.js
@@ -11,8 +11,22 @@ function httpGetAllLaunches(req, res) {
 // Function for handling POST of new launches
 function httpAddNewLaunch(req, res) {
     const launch = req.body;
+
+    // Validate all of our values exist
+    if (!launch.mission || !launch.rocket || !launch.date 
+          || !launch.destination) {
+            return res.status(400).json({
+                error: 'Missing required launch property'
+            })
+        }
     // Since we cannot pass date objects from our json, we will convert the passed in date string to a Date object here
     launch.launchDate = new Date(launch.launchDate);
+    // Validate that our date is a valid date
+    if (isNaN(launch.launchDate)) {
+        return res.status(400).json({
+            error: 'Invalid launch date'
+        })
+    };
 
     addNewLaunch(launch);
     return res.status(201).json(launch);
