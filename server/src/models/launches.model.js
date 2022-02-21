8af7f753842a46b2767a3eba3af9df2fc7fd9f03
jam-madrigal <jam.madrigal@gmail.com>
2022-02-20 17:25:08 -0800

    Add launches model

---
+++ server/src/models/launches.model.js
@@ -0,0 +1,11 @@
+// Creating an object that defines our launch data
+const launch = {
+    flightNumber: 100,
+    mission: 'Kepler Exploration X',
+    rocket: 'Explorer IS1',
+    launchDate: new Date('December 27, 2030'),
+    destination: 'Kepler-442 b',
+    customers: ['NASA', 'ZTM'],
+    upcoming: true,
+    success: true
+};
