7f02d96d0a11e370e744e9512152c93d43338c99
jam-madrigal <jam.madrigal@gmail.com>
2022-02-21 17:44:14 -0800

    Add httpSubmitLaunch function to try catch block

--- client/src/hooks/requests.js
+++ client/src/hooks/requests.js
@@ -19,13 +19,19 @@ async function httpGetLaunches() {
 
 // Submit given launch data to launch system.
 async function httpSubmitLaunch(launch) {
-  return await fetch(`${API_URL}/launches`, {
-    method: "post",
-    headers: {
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify(launch)
-  })
+  try {
+    return await fetch(`${API_URL}/launches`, {
+      method: "post",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(launch)
+    })
+  } catch(err) {
+    return {
+      ok: false
+    }
+  }
 }
 
 async function httpAbortLaunch(id) {
