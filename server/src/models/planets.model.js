4130592961b6117d1fa351d750e5b204a0280e17
jam-madrigal <jam.madrigal@gmail.com>
2022-02-27 23:09:51 -0800

    Update getAllPlanets() to async and mongoose

--- server/src/models/planets.model.js
+++ server/src/models/planets.model.js
@@ -46,8 +46,10 @@ function loadPlanetsData() {
     });
 }
     
-function getAllPlanets() {
-    return habitablePlanets;
+async function getAllPlanets() {
+    return await planets.find({
+
+    });
 }
 
 module.exports = {
--- server/src/routes/planets/planets.controller.js
+++ server/src/routes/planets/planets.controller.js
@@ -1,7 +1,7 @@
 const { getAllPlanets } = require('../../models/planets.model');
 
 // Return our model of planets
-function httpGetAllPlanets(req, res) {
+async function httpGetAllPlanets(req, res) {
     return res.status(200).json(getAllPlanets());
 }
 
